01ebb7c197adba679cd15e8c07db216a99fc833a
tanmoysrt <57363826+tanmoysrt@users.noreply.github.com>
2025-03-07 13:50:59 +0530

    feat: In stages api send info of stage-0 for guest users

--- ctf/api/__init__.py
+++ ctf/api/__init__.py
@@ -1,4 +1,5 @@
 import json
+from typing import Literal
 
 import frappe
 import frappe.utils
@@ -11,8 +12,27 @@ def status():
 	return {"setup_completed": is_setup_completed(), "ctf_status": get_ctf_status()}
 
 
-@frappe.whitelist()
+@frappe.whitelist(allow_guest=True)
 def stages():
+	if frappe.session.user == "Guest":
+		return [
+			{
+				"stage": "STAGE-0",
+				"title": "Register to CTF",
+				"description": """This is the first step towards the CTF.
+
+You will find a unique link somewhere in the page to register to this contest.
+""",
+				"points": 0,
+				"variables": {},
+				"submitted": False,
+				"submitted_flag": "",
+				"correct": False,
+				"status": "Not Registered",
+				"show_input": False,
+			}
+		]
+
 	if not is_setup_completed():
 		return []
 
@@ -26,29 +46,34 @@ def stages():
 			"FLAG_PAGE_ROUTE": "route to the flag page",
 			"FLAG_CHARACTERS": "characters to be replaced in the flag page",
 		},
-		"solved": false,
+		"submitted": false,
 		"correct": false,
 	}
 	"""
 
+	ctf_active = is_ctf_active()
 	stages = frappe.get_all("CTF Stage", fields=["title", "description", "points", "name"], order_by="name")
-	candidate_satges = frappe.get_all(
+	candidate_stages = frappe.get_all(
 		"CTF Candidate Stage",
 		fields=["stage", "variables", "submitted_flag", "correct"],
 		filters={"parent": current_ctf_candidate()},
 	)
 	for stage in stages:
 		candidate_stage = None
-		for s in candidate_satges:
+		for s in candidate_stages:
 			if s.stage == stage.name:
 				candidate_stage = s
 				break
 		if not candidate_stage:
 			frappe.throw("Something is wrong. Please contact CTF admin.")
 
-		stage["solved"] = bool(candidate_stage.submitted_flag and len(candidate_stage.submitted_flag) > 0)
+		stage["submitted"] = bool(candidate_stage.submitted_flag and len(candidate_stage.submitted_flag) > 0)
 		stage["correct"] = bool(candidate_stage.correct)
+		stage["status"] = (
+			"Not Submitted" if not stage["submitted"] else "Correct" if stage["correct"] else "Incorrect"
+		)
 		stage["variables"] = json.loads(candidate_stage.variables)
+		stage["show_input"] = ctf_active
 
 	return stages
 
@@ -73,5 +98,9 @@ def is_setup_completed() -> bool:
 	return frappe.get_value("CTF Candidate", current_ctf_candidate(), "setup_completed")
 
 
-def get_ctf_status() -> dict[str, bool]:
+def get_ctf_status() -> Literal["Not Started", "Started", "Ended"]:
 	return frappe.get_value("CTF Settings", "CTF Settings", "status")
+
+
+def is_ctf_active() -> bool:
+	return get_ctf_status() == "Started"
