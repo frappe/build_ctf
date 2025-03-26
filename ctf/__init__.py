8830660627636657ecc133b7e9936a8a537af0ce
Ankush Menat <ankush@frappe.io>
2025-03-26 09:59:18 +0530

    fix: Monkey patch exception checks

--- ctf/__init__.py
+++ ctf/__init__.py
@@ -1,1 +1,20 @@
 __version__ = "0.0.1"
+
+import frappe
+from frappe.utils import response
+
+# Need to patch exception checker
+
+
+def _allow_website_users():
+	# from frappe.permissions import is_system_user
+
+	return (
+		frappe.db
+		# and frappe.get_system_settings("allow_error_traceback")
+		and (not frappe.local.flags.disable_traceback or frappe._dev_server)
+		# and is_system_user()
+	)
+
+
+response.is_traceback_allowed = _allow_website_users
